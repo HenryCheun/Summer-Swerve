7877d9e8f98ab61ade386c13b3eab3570cd9cc6b
ojlennon <45679125+ojlennon@users.noreply.github.com>
2023-12-06 09:39:38 -0700

    Removing Preseason Stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,15 +22,10 @@ import frc.controlschemes.MechanismScheme;
 import frc.controlschemes.SwerveDriveScheme;
 import frc.controlschemes.Testing;
 import frc.maps.RobotMap;
-import frc.robot.autonomous.Balance;
-import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.SwerveDrive;
 
 public class RobotContainer {
     SwerveDrive swerveDrive = new SwerveDrive();
-    Intake intake = new Intake();
-    Arm arm = new Arm();
     /** Event map for path planner */
     public static HashMap<String, Command> eventMap = new HashMap<>();
     /** Command List for auto paths in SmartDashBoard */
@@ -60,9 +55,10 @@ Field2d ff;
     
     public RobotContainer() {
         SwerveDriveScheme.configure(swerveDrive, 0);
-        MechanismScheme.configure(intake, arm);
         // Testing.configure(swerveDrive, 0);
-        eventMap.put("toggle", Commands.runOnce(() -> swerveDrive.toggleEvent(), intake));
+
+        //fix this with a new subsystem
+        eventMap.put("toggle", Commands.runOnce(() -> swerveDrive.toggleEvent(), null ));
 
 
         diagnosticsInit();
@@ -73,9 +69,11 @@ Field2d ff;
         for (String pathName : paths) {
             autoCommands.addOption(pathName, followPathPlanner(pathName).withName(pathName));
         }
-        autoCommands.addDefaultOption("Nothing", Commands.run(() -> swerveDrive.printWorld(), swerveDrive).withName("Nothing"));
+        //this is the default command to prevent accidents of running the wrong auto
+        autoCommands.addDefaultOption("Nothing",
+                Commands.run(() -> swerveDrive.printWorld(), swerveDrive).withName("Nothing"));
+        
         SmartDashboard.putData("Auto", autoCommands.getSendableChooser());
-        autoCommands.addOption("balance", new Balance(swerveDrive));
         autoCommands.addOption("Move Straight", swerveDrive.moveCommand().withName("Move Straight"));
         
         Shuffleboard.getTab("Diagnostics").add("SwerveDrive", swerveDrive);
